34e03050448726818fe1861251265ce76f35c29d
sangcomz <sangcomz@naver.com>
2015-04-07 14:42:37 +0900

    다시 돌려놓음

--- app/src/main/java/kr/co/sangcomz/dynamicfragment/MainActivity.java
+++ app/src/main/java/kr/co/sangcomz/dynamicfragment/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -16,6 +17,9 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        et = (EditText)findViewById(R.id.et_num);
+        btn = (Button)findViewById(R.id.btn);
+
     }
 
 }
